62a1d4d3336f116c97fa733c3f0db536fb6a9b69
Jaya Dubey <jayadubey6402@gmail.com>
2024-07-10 02:47:56 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import 'tailwindcss/tailwind.css'
 export default function Home() {
   return (
     <>
-    <h1>Hello World!</h1>
     </>
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
